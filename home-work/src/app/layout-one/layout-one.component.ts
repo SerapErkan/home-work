ae9ee37de0337c753919d936edb811fb0fa92a37
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2023-01-08 22:43:59 +0300

    grid-layout/design

--- home-work/src/app/layout-one/layout-one.component.ts
+++ home-work/src/app/layout-one/layout-one.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../libs';
 
 @Component({
   selector: 'layout-one',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutOneComponent implements OnInit {
 
-  constructor() { }
+  bgcolor$:any;
+  
+  constructor(private local:LocalStorageService) {
+  // I-yol
+  // local.modeSubject.subscribe(data=>{
+  //  this.color=data;
+  // });
+  this.bgcolor$=local.modeDataSubjec;
+   }
+   ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
+   }
 
 }
