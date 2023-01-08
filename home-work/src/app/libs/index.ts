ff781838479cb2dcb14be12bb84259c52041f584
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2023-01-08 21:12:12 +0300

    model

---
+++ home-work/src/app/libs/index.ts
@@ -0,0 +1,4 @@
+import { from } from "rxjs"
+
+export * from "./model/form-model";
+export * from "./services/local-storage.service";
---
+++ home-work/src/app/libs/model/form-model.ts
@@ -0,0 +1,6 @@
+export interface FormModel {
+    description:string;
+    fname:string;
+    lname:string;
+    password:string;
+}
