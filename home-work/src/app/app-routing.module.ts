3212011811db9124e620fa3dc6d9592195916753
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2023-01-08 23:30:00 +0300

    routing-onePage-twoPage

--- home-work/src/app/app-routing.module.ts
+++ home-work/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutOneComponent } from './layout-one/layout-one.component';
+import { LayoutTwoComponent } from './layout-two/layout-two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "",
+    component: LayoutOneComponent
+  },
+  {
+    path: "one",
+    component: LayoutOneComponent
+  },
+  {
+    path: "two",
+    component: LayoutTwoComponent
+  },
+
+  {
+    path: "data",
+    loadChildren: () => import('./data/data.module')
+      .then(m => m.DataModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
