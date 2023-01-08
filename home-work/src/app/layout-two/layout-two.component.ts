a0bc2692d5a5b25d664c189c1e0787642dac08c7
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2023-01-08 22:16:53 +0300

    layout-header-footer -bgColor

--- home-work/src/app/layout-two/layout-two.component.ts
+++ home-work/src/app/layout-two/layout-two.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../libs';
 
 @Component({
   selector: 'layout-two',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutTwoComponent implements OnInit {
 
-  constructor() { }
+  bgcolor$:any;
+  
+  constructor(private local:LocalStorageService) {
+  // I-yol
+  // local.modeSubject.subscribe(data=>{
+  //  this.color=data;
+  // });
+  this.bgcolor$=local.modeDataSubjec;
+   }
+   ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
-
+   }
 }
